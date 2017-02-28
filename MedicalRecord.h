c383cf8b8e9429c102593e833b61d4963eb14431
Jackie Lo <jacksl@csu.fullerton.edu>
2017-02-27 20:46:51 -0800

    Add files via upload

--- MedicalRecord.h
+++ MedicalRecord.h
@@ -1,6 +1,8 @@
 #pragma once
+#include <iostream>
 #include <string>
 #include <stdexcept>
+#include <fstream>
 
 #include "Baby.h"
 
@@ -9,25 +11,34 @@ using namespace std;
 class MedicalRecord {
 public:
 	// default constructor
-	MedicalRecord() {
-		// TO BE COMPLETED
+	MedicalRecord() 
+	{
+		totalBirths = 1000;
+		saveData = new Baby[totalBirths];
+		NumofB = 0;
 	}
 
 	// destructor
-	~MedicalRecord() {
-		// TO BE COMPLETED
+	~MedicalRecord() 
+	{
+		delete[] saveData;
+
+		cout << "Object is being deleted" << endl;
 	}
 
 	// Load information from a text file with the given filename.
-	void buildMedicalRecordfromDatafile(string filename) {
+	void buildMedicalRecordfromDatafile(string filename) 
+	{
 		ifstream myfile(filename);
 
-		if (myfile.is_open()) {
+		if (myfile.is_open()) 
+		{
 			cout << "Successfully opened file " << filename << endl;
 			string name;
 			int weight;
-			while (myfile >> name >> weight) {
-				// cout << name << " " << weight << endl;
+			while (myfile >> name >> weight) 
+			{
+				cout << name << " " << weight << endl;
 				Baby b(name, weight);
 				addEntry(b);
 			}
@@ -38,32 +49,76 @@ public:
 	}
 
 	// return the most frequently appearing name in the text file
-	string mostPopularName() {
-		return "COMPLETE ME"; // TO BE COMPLETED
+	string mostPopularName() 
+	{
+		int nameCount = 0;
+		int previous = 0;
+		string totalofName;
+
+		for (int i = 0; i < NumofB; i++)
+		{
+			for (int j = 0; j < NumofB; j++)
+			{
+				if (saveData[i].getName() == saveData[j].getName())
+				{
+					nameCount++;
+				}
+			}
+			if (nameCount > previous)
+			{
+				totalofName = saveData[i].getName();
+				previous = nameCount;
+				nameCount = 0;
+			}
+		}
+		return totalofName;
 	}
 
 	// return the number of baby records loaded from the text file
-	int numberOfBirths() {
-		return -1; // TO BE COMPLETED
+	int numberOfBirths() 
+	{	
+		return NumofB;
 	}
 
 	// return the number of babies who had birth weight < 2500 grams
-	int numberOfBabiesWithLowBirthWeight() {
-		return -1; // TO BE COMPLETED
+	int numberOfBabiesWithLowBirthWeight() 
+	{
+		int lowBweight = 0;
+
+		for (int i = 0; i < NumofB; i++)
+		{
+			if (saveData[i].getWeight() < 2500)
+			{
+				lowBweight++;
+			}
+		}
+		return lowBweight;
 	}
 
 	// return the number of babies who have the name contained in string s
-	int numberOfBabiesWithName(string s) {
-		return -1; // TO BE COMPLETED
+	int numberOfBabiesWithName(string s) 
+	{
+		int numofSamename = 0;
+
+		for (int i = 0; i < NumofB; i++)
+		{
+			if (saveData[i].getName() == s)
+			{
+				numofSamename++;
+			}
+		}
+		return numofSamename;
 	}
 
 private:
 	// update the data structure with information contained in Baby object
-	void addEntry(Baby b) {
-		// TO BE COMPLETED
+	void addEntry(Baby b) 
+	{
+		saveData[NumofB] = b;
+		NumofB++;
 	}
 
-	// Add private member variables for your data structure along with any 
-	// other variables required to implement the public member functions
-
+	Baby* saveData;
+	int NumofB;
+	int totalBirths;
 };
